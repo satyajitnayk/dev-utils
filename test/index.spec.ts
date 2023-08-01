916ff2fb8998dc38b8f1e1c494a1302457644391
satyajit nayak <satyajitn15@gmail.com>
2023-08-01 22:41:53 +0530

    Add test cases for withTempDirs

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,8 @@
 import {replaceMultiple, withTempDirs} from '../src'
+import * as fs from 'fs';
+
 
 describe('util functions', () => {
-  // TODO: Add testcases
-  it('withTempDirs', () => {
-    expect(1).toBe(1)
-  })
-
   describe('replaceMultiple', function () {
     it('equal lengths of matchStrings and replacementStrings', () => {
       expect(replaceMultiple(
@@ -42,5 +39,62 @@ describe('util functions', () => {
         [])
       ).toBe('')
     })
+  });
+  describe('withTempDirs', () => {
+    it('should return the result of the callback function', async () => {
+      const result = await withTempDirs(['/tmp/dir1'], async () => {
+        return 42;
+      });
+      expect(result).toBe(42);
+    });
+    it('should handle asynchronous operations in the callback', async () => {
+      const result = await withTempDirs(['/tmp/dir1'], async () => {
+        await new Promise(resolve => setTimeout(resolve, 100)); // Simulate an async operation
+        return 'done';
+      });
+      expect(result).toBe('done');
+    });
+    it('should throw an error if the callback throws an error', async () => {
+      await expect(
+        withTempDirs(['/tmp/dir1'], async () => {
+          throw new Error('Something went wrong!');
+        })
+      ).rejects.toThrow('Something went wrong!');
+    });
+    it('should create the temporary directories before executing the callback', async () => {
+      const dirs = ['/tmp/dir1'];
+      await withTempDirs(dirs, async () => {
+        expect(await fs.promises.readdir(dirs[0])).toEqual([]); // Check if directory is empty
+      });
+    });
+    it('should delete the temporary directories after executing the callback', async () => {
+      const dirs = ['/tmp/dir1'];
+      await withTempDirs(dirs, async () => {
+        // Do nothing in the callback
+      });
+      await expect(async () => await fs.promises.access(dirs[0]))
+        .rejects.toThrow("ENOENT: no such file or directory, access '/tmp/dir1'") // Check if directory exists
+    });
+    it('should work with multiple temporary directories', async () => {
+      const dirs = ['/tmp/dir1', '/tmp/dir2'];
+      const result = await withTempDirs(dirs, async () => {
+        return dirs.join(', ');
+      });
+      expect(result).toBe('/tmp/dir1, /tmp/dir2');
+    });
+    it('should handle nested temporary directories', async () => {
+      const dirs = ['/tmp/dir1/subdir'];
+      const result = await withTempDirs(dirs, async () => {
+        return 'nested';
+      });
+      expect(result).toBe('nested');
+    });
+    it('should work with an empty array of temporary directories', async () => {
+      const result = await withTempDirs([], async () => {
+        return 'empty';
+      });
+
+      expect(result).toBe('empty');
+    });
   });
 })
