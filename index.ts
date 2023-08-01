b97d559e241029909b3b0fd47c7d1df82133c4ac
satyajit nayak <satyajitn15@gmail.com>
2023-08-01 11:50:34 +0530

    initial setup

---
+++ index.ts
@@ -0,0 +1,48 @@
+import fs from 'fs';
+
+/**
+ * creates temporary directories, execute provides function
+ * & clean all directories thereafter
+ * @param dirs
+ * @param callback
+ */
+export async function withTempDirs<T>(
+  dirs: string[],
+  callback: () => Promise<T>
+): Promise<T> {
+  try {
+    // create temporary directories
+    await Promise.all(
+      dirs.map(dir => fs.promises.mkdir(dir, {recursive: true}))
+    )
+    // execute the callback function
+    return await callback();
+  } catch (e) {
+    throw e;
+  } finally {
+    // clean all the temporary directories
+    await Promise.all(
+      dirs.map(dir => fs.promises.rmdir(dir, {recursive: true}))
+    )
+  }
+}
+
+/**
+ * replace all match strings with replacement strings in str
+ * @param str
+ * @param matchStrings
+ * @param replacementStrings
+ */
+export function replaceMultiple(
+  str: string,
+  matchStrings: string[],
+  replacementStrings: string[]
+): string {
+  if (matchStrings.length !== replacementStrings.length) {
+    return str;
+  }
+  matchStrings.forEach((matchStr: string, index: number) => {
+    str = str.replace(matchStr, replacementStrings[index]);
+  })
+  return str;
+}
